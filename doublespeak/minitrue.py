5bb48c0d3550cfdb4eafa4f7994ec9765f9a3d32
ihavespoons <bgittins22@gmail.com>
2017-04-05 23:16:23 +1000

    Modified class naming structure to newspeak for getter function and began working on search encoding.

--- doublespeak/minitrue.py
+++ doublespeak/minitrue.py
@@ -4,11 +4,13 @@ import sys
 import urllib
 import hashlib
 
-# Take cli input
-search_arg = sys.argv[1]
 
+def dispatcher ():
+    # Take cli input
+    search_arg = sys.argv[1]
 # Encode Cli input into
-hash_sha1 = hashlib.sha1(search_arg.encode('utf-8'))
-hash_result = hash_sha1.hexdigest()
+    hash_sha1 = hashlib.sha1(search_arg.encode('utf-8'))
+    hash_result = hash_sha1.hexdigest()
+    return
 
 
