025470fb2df448cac1e66ef8c202e9c6abcdb604
ben.gittins <ben.gittins@xtreme.com.au>
2017-04-07 16:49:16 +1000

    Began work on new searchlight function.

--- doublespeak/minitrue.py
+++ doublespeak/minitrue.py
@@ -5,12 +5,16 @@ import urllib
 import hashlib
 
 
-def dispatcher ():
+def hasher():
     # Take cli input
     search_arg = sys.argv[1]
-# Encode Cli input into
+    # Encode Cli input into
     hash_sha1 = hashlib.sha1(search_arg.encode('utf-8'))
     hash_result = hash_sha1.hexdigest()
-    return
+    return hash_result
+
+def searchlight(hashresult):
+    # takes a hash of the search and transmits it to the search api
+
 
 
