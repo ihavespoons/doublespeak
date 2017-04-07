b85682f77c48e70ecfa36f40f753a1dc6328e914
ihavespoons <bgittins22@gmail.com>
2017-04-07 20:54:48 +1000

    Further adjusted verisficator.

--- doublespeak/versificator.py
+++ doublespeak/versificator.py
@@ -1,5 +1,7 @@
 import ipgetter
 import json
+import urllib
+
 from geoip import geolite2
 from doublespeak.constants import *
 
@@ -10,6 +12,8 @@ def versificator():
 
     if(match is not None):
      country = match.country
+     with urllib.request.urlopen("http://doublespeak.io/api/serverlist") as url:
+        data = json.loads(url.read().decode())
 
     else:
         return(DEF_SERVER)
